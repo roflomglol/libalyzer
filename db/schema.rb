2ee56976c3e993668f4efc02fb8e95b49c708c2b
Sergey Yazan <sergey.yazan@gmail.com>
2016-12-29 16:20:29 +0300

    adds report model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161209121741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reports", force: :cascade do |t|
+    t.json "body"
+    t.text "raw"
+  end
 
 end
