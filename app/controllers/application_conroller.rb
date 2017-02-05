4b781cc58a6ea7a7bea49c50d8e1918dc7a774bd
Sergey Yazan <sergey.yazan@gmail.com>
2017-02-05 20:11:44 +0300

    reports#show beta views

--- app/controllers/application_conroller.rb
+++ app/controllers/application_conroller.rb
@@ -29,6 +29,7 @@ module Libalyzer
 
     get '/reports/:id' do
       @report = Report.find(params[:id])
+      @report_as_data_attribute = @report.body.map { |k, v| { axis: k, value: v } }.to_json
 
       slim :'reports/show', layout: :'layouts/application'
     end
